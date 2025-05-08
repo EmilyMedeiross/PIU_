25e36df570d0402fd26943f516372e0e06a95841
Emily Medeiros <161541242+EmilyMedeiross@users.noreply.github.com>
2025-05-08 16:50:02 -0300

    Add files via upload

---
+++ aula1/src/FirstComponent.jsx
@@ -0,0 +1,17 @@
+function FirstComponent () {
+
+    const nome = "emily"
+    
+    return (
+        <>
+            <div>
+                 <h1>Hello, world!</h1>
+            </div>
+            <div>
+                <p> Seu nome é {nome}</p>
+            </div>
+        </>  
+    )
+}
+
+export default FirstComponent
