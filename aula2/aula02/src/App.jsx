1d560f0251196a39eb364f82ac5174a8ac38cb73
EmilyMedeiross <emilysantosjprn@gmail.com>
2025-05-15 17:55:14 -0300

    Aula02

---
+++ aula2/aula02/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Escopo from './componentes/Escopo'
+import EscopoReduz  from './componentes/EscopoReduz'
+import Info1v from './componentes/info1v'
+
+function App() {
+
+  return (
+    <>
+       <Escopo/>
+       <Info1v/>
+    </>
+  )
+}
+export default App
